f26f96772f7dd5ac30662b843eef254ec6679ac4
ichi <tmy12171998@gmail.com>
2021-09-12 19:45:54 +0900

    make task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { FormControl, List, TextField } from '@material-ui/core';
+import  AddToPhotosIcon  from '@material-ui/icons/AddToPhotos';
 import React,{useState, useEffect} from 'react';
 import './App.css';
 import { db } from './firebase';
+import { TaskItem } from './TaskItem';
 
 const App: React.FC = () => {
-  const [tasks, setTasks] = useState([{id:"", title:""}])
+  const [tasks, setTasks] = useState([{id:"", title:""}]);
+  const [input, setInput] = useState('');
   useEffect(()=>{
     const unSub = db.collection("tasks").onSnapshot((snapshot)=>{
       setTasks(
@@ -13,11 +17,35 @@ const App: React.FC = () => {
     return () => unSub();
   },[])
 
+  const newTask = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    db.collection("tasks").add({title: input})
+    setInput('')
+  }
+
   return (
     <div className="App">
-      {
-        tasks.map((task)=><h3 key={task.id}>{task.title}</h3>)
-      }
+      <h1>Todo App by React/Firebase</h1>
+      <FormControl>
+        <TextField
+        InputLabelProps={{
+          shrink: true,
+        }}
+          label="New task ?"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>)=>setInput(e.target.value)}
+        />
+      </FormControl>
+      <button disabled={!input} onClick={e=>newTask(e)}>
+        <AddToPhotosIcon />
+      </button>
+
+      <List>
+        {
+          tasks.map((task)=>(
+            <TaskItem key={task.id} id={task.id} title={task.title}/>
+          ))
+        }
+      </List>
     </div>
   );
 }
